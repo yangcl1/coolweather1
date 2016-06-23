b2a5662f1e9d64365a2aac92ff70c8535beedd72
unknown <>
2016-06-23 14:44:57 +0800

    First commit

---
+++ src/com/coolweather1/app/model/Province.java
@@ -0,0 +1,27 @@
+package com.coolweather1.app.model;
+
+public class Province {
+	private int Id;
+	private String provinceName;
+	private String provinceCode;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int Id) {
+		this.Id = Id;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+
+}
